3da834dd78363adc2a0ac892058bbacbb4c383b6
mgadzhi <musaevgadzhi@gmail.com>
2016-04-12 23:01:45 +0200

    add utils module with combinations() function

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+def combinations(*args):
+    if len(args) == 1:
+        return [[x] for x in args[0]]
+    combs = []
+    for item in args[0]:
+        for x in combinations(*args[1:]):
+            combs.append([item] + x)
+    return combs
