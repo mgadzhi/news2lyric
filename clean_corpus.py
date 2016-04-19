29e819c15bf30942266a1a7e7abd754784945c41
mgadzhi <musaevgadzhi@gmail.com>
2016-04-19 21:08:36 +0200

    finish script clean_corpus

--- clean_corpus.py
+++ clean_corpus.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import re
 import sys
@@ -14,17 +15,25 @@ def contains_url(line):
     match = re.search(pattern, line)
     return match is not None
 
+
 def replace_and(line):
     return line.replace('&', ' and ')
 
 def delete_punctuation(line):
-    pattern = '[\.?!\-#£\*%,&_<>°]+'
-    new_line = re.sub(pattern, '', line)
+    pattern = '[\.?!\#£\*%,&_<>°":;\(\)/\[\]]+'
+    new_line = re.sub(pattern, ' ', line)
     return new_line
 
 
 def deal_with_apostrophe(line):
-    pass
+    pattern = r"^'|\s'|'\s|'$"
+    new_line = re.sub(pattern, ' ', line)
+    return new_line
+
+def deal_with_dash(line):
+    pattern = r"^-|\s-|-\s|-$"
+    new_line = re.sub(pattern, ' ', line)
+    return new_line
 
 
 def lowercase(line):
@@ -36,6 +45,11 @@ def main():
     lines_without_url = (line for line in lines if not contains_url(line))
     output_replace_and = (replace_and(line) for line in lines_without_url)
     output_delete_punctuation = (delete_punctuation(line) for line in output_replace_and)
+    output_deal_with_apostrophe = (deal_with_apostrophe(line) for line in output_delete_punctuation)
+    output_deal_with_dash = (deal_with_dash(line) for line in output_deal_with_apostrophe)
+    output_lowercase = (lowercase(line) for line in output_deal_with_dash)
+    for line in output_lowercase:
+        sys.stdout.write(line)
 
 
 if __name__ == '__main__':
