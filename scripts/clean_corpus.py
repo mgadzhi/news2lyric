1e4a0120feb3a7036d00d3eca3af7ffad8038edb
mgadzhi <musaevgadzhi@gmail.com>
2016-04-23 17:08:36 +0200

    Add some unicode punctuation symbols to be cleaned off

--- scripts/clean_corpus.py
+++ scripts/clean_corpus.py
@@ -1,37 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import codecs
 import re
 import sys
-from urlparse import urlparse
 
 
 def contains_number(line):
-    match = re.search('[0-9]+', line)
+    match = re.search(u'[0-9]+', line)
     return match is not None
 
 
 def contains_url(line):
-    pattern = '(://)?.*\.[a-zA-Z]{2,3}/?'
+    pattern = u'(://)?.*\.[a-zA-Z]{2,3}/?'
     match = re.search(pattern, line)
     return match is not None
 
 
 def replace_and(line):
-    return line.replace('&', ' and ')
+    return line.replace(u'&', u' and ')
+
 
 def delete_punctuation(line):
-    pattern = '[\.?!\#£\*%,&_<>°":;\(\)/\[\]]+'
+    pattern = u'[\.?!\#£\*%,&_<>°":;\(\)/\[\]“”—]+'
     new_line = re.sub(pattern, ' ', line)
     return new_line
 
 
 def deal_with_apostrophe(line):
-    pattern = r"^'|\s'|'\s|'$"
+    pattern = u"^'|\s'|'\s|'$"
     new_line = re.sub(pattern, ' ', line)
     return new_line
 
+
 def deal_with_dash(line):
-    pattern = r"^-|\s-|-\s|-$"
+    pattern = u"^-|\s-|-\s|-$"
     new_line = re.sub(pattern, ' ', line)
     return new_line
 
@@ -40,16 +42,25 @@ def lowercase(line):
     return line.lower()
 
 
+def remove_doublespace(line):
+    return re.sub(u'\s+', ' ', line.strip())
+
+
 def main():
-    lines = (line for line in sys.stdin if not contains_number(line))
+    source = codecs.getreader(u'utf-8')(sys.stdin)
+    sink = codecs.getwriter(u'utf-8')(sys.stdout)
+
+    lines = (line.strip() for line in source if not contains_number(line))
     lines_without_url = (line for line in lines if not contains_url(line))
     output_replace_and = (replace_and(line) for line in lines_without_url)
     output_delete_punctuation = (delete_punctuation(line) for line in output_replace_and)
     output_deal_with_apostrophe = (deal_with_apostrophe(line) for line in output_delete_punctuation)
     output_deal_with_dash = (deal_with_dash(line) for line in output_deal_with_apostrophe)
     output_lowercase = (lowercase(line) for line in output_deal_with_dash)
-    for line in output_lowercase:
-        sys.stdout.write(line)
+    output_doublespaces = (remove_doublespace(line) for line in output_lowercase)
+    for line in output_doublespaces:
+        # sys.stdout.write(line.decode('utf-8'))
+        sink.write(line.strip() + u'\n')
 
 
 if __name__ == '__main__':
