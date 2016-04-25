7018de3356756852364c84e5690e0c61e3adb1f3
mgadzhi <musaevgadzhi@gmail.com>
2016-04-25 22:11:23 +0200

    Add structures.py with a structure for guinea pig

---
+++ news2lyric/structures.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+u"""
+    "Structure" is represented as two-tuple:
+    1. List of stresses for every line in the poem
+    2. List of rhyming lines. Use the same number for those lines
+       that should rhyme.
+"""
+
+guinea_pig = (
+    [[False, True, False, True, False, True, False, True],
+     [False, True, False, True, False, True, False, True],
+     [False, True, False, True, False, True, False, True],
+     [False, True, False, True, False, True, False, True]],
+    [1, 1, 2, 2],
+)
