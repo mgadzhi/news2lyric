a51a78b924d4ce383a8b5787944bf5b1c508e38c
AnneleenArtois <anneleen123@hotmail.com>
2016-04-29 23:56:30 +0200

    add delete_dots_end_sentence(line) :cactus:

--- scripts/clean_corpus.py
+++ scripts/clean_corpus.py
@@ -15,21 +15,22 @@ def split_on_new_line(line):
 # Enumerations such as 1. (digit + dot) will not be deleted since they can be line endings.
 # These sentences will be omitted in contains_number(line)
 def delete_enumeration_digit(line):
-	pattern = re.search(u'[0-9]{1,2}\)')
-	new_line = re.sub(pattern, ' ', line)
-	return new_line
+    pattern = (u'[0-9]{1,2}\)')
+    new_line = re.sub(pattern, ' ', line)
+    return new_line
 
 
 def delete_enumeration_character(line):
-	pattern = re.search(u'[a-zA-Z]{1}\)')
-	new_line = re.sub(pattern, ' ', line)
-	return new_line	
-	
-	
+    pattern = (u'[a-zA-Z]{1}\)')
+    new_line = re.sub(pattern, ' ', line)
+    return new_line
+
+
 # Lines containing numbers will be omittted
 def contains_number(line):
     match = re.search(u'[0-9]+', line)
     return match is not None
+
 
 # Lines containing urls will be omitted
 def contains_url(line):
@@ -43,7 +44,13 @@ def replace_and(line):
 
 
 def delete_punctuation(line):
-    pattern = u'[\.?!\#£\*%,&_<>°":;\(\)/\[\]“”—]+'
+    pattern = u'[?!\#£\*%,&_<>°":;\(\)/\[\]“”—]+'
+    new_line = re.sub(pattern, ' ', line)
+    return new_line
+
+
+def delete_dots_end_sentence(line):
+    pattern = u"[\.]+$"
     new_line = re.sub(pattern, ' ', line)
     return new_line
 
@@ -69,17 +76,19 @@ def remove_doublespace(line):
 
 
 def main():
-    source = codecs.getreader(u'utf-8')(sys.stdin)
-    sink = codecs.getwriter(u'utf-8')(sys.stdout)
-
-	# if necessary, split_on_new_line
+    #source = codecs.getreader(u'utf-8')(sys.stdin)
+    source = codecs.open('tryout.txt', 'r', 'utf-8')
+    #sink = codecs.getwriter(u'utf-8')(sys.stdout)
+    sink = codecs.open('zzz.txt', 'w', 'utf-8')
+    # if necessary, split_on_new_line
     lines_without_enumeration_digits = (delete_enumeration_digit(line) for line in source)
-	lines_without_enumeration_charachters = (delete_enumeration_character(line) for line in lines_without_enumeration_digits)
-	lines_without_numbers = (line.strip() for line in lines_without_enumeration_charachters if not contains_number(line))
-    lines_without_url = (line for line in lines if not contains_url(line))
+    lines_without_enumeration_charachters = (delete_enumeration_character(line) for line in lines_without_enumeration_digits)
+    lines_without_numbers = (line.strip() for line in lines_without_enumeration_charachters if not contains_number(line))
+    lines_without_url = (line for line in lines_without_numbers if not contains_url(line))
     output_replace_and = (replace_and(line) for line in lines_without_url)
     output_delete_punctuation = (delete_punctuation(line) for line in output_replace_and)
-    output_deal_with_apostrophe = (deal_with_apostrophe(line) for line in output_delete_punctuation)
+    output_delete_dots_end_sentence = (delete_dots_end_sentence(line) for line in output_delete_punctuation)
+    output_deal_with_apostrophe = (deal_with_apostrophe(line) for line in output_delete_dots_end_sentence)
     output_deal_with_dash = (deal_with_dash(line) for line in output_deal_with_apostrophe)
     output_lowercase = (lowercase(line) for line in output_deal_with_dash)
     output_doublespaces = (remove_doublespace(line) for line in output_lowercase)
