ec4b564dd801233f8f86cf18cafb8414b1ea7b85
mgadzhi <musaevgadzhi@gmail.com>
2016-05-03 18:44:33 +0200

    Refactoring. extract_matching_lines functionality is moved to separate function

--- news2lyric/validate.py
+++ news2lyric/validate.py
@@ -7,7 +7,7 @@ from utils import combinations, flatten
 import phonetics as ph
 
 
-#This function should be moved somewhere else in the future
+#This function should be put somewhere else in the future
 def get_phonetic_combinations(words):
     line_phonemes = []
     for word in words:
@@ -24,6 +24,13 @@ def validate_line(phonemes, stress_structure):
     return ph.stress_mask(phonemes) == stress_structure
 
 
+def extract_matching_phonemes_line(ph_lines, stress_structure):
+    for line in ph_lines:
+        if validate_line(line, stress_structure):
+            return line
+    return None
+
+
 def validate_rhymes(lines, rhyme_structure):
     d = {}
     for line, rhyme_class in izip_longest(lines, rhyme_structure):
@@ -38,12 +45,26 @@ def validate_rhymes(lines, rhyme_structure):
 def validate_couplet(couplet, structure):
     ph_combinations = (get_phonetic_combinations(line.split(' ')) for line in couplet)
     ph_lines = [map(flatten, line) for line in ph_combinations]
-    stress_lines = (map(ph.stress_mask, phonemes) for phonemes in ph_lines)
-    lines_masks_zip = izip_longest(stress_lines, structure[0])
+    ph_lines_stresses = izip_longest(ph_lines, structure[0])
 
-    stresses_match = all(b in a for a, b in lines_masks_zip)
-    rhymes_match = validate_rhymes(ph_lines, structure[1])
+    matching_lines = [extract_matching_phonemes_line(line, stresses) for line, stresses in ph_lines_stresses]
+
+    stresses_match = len(matching_lines) == len(couplet)
+    rhymes_match = validate_rhymes(matching_lines, structure[1])
 
     return stresses_match and rhymes_match
+
+
+if __name__ == '__main__':
+    from news2lyric.structures import guinea_pig
+    import itertools
+    with open('data/guinea_pig.2.clean.txt') as source:
+        ngrams = (ng.strip() for ng in source)
+        couplets = itertools.permutations(ngrams, 4)
+        for couplet in couplets:
+            if validate_couplet(couplet, guinea_pig):
+                print '\n'.join(couplet)
+                print '---'
+
 
 
