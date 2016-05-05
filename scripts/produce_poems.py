c3a30f15ef62f32cef87384856b4d2b3ae5d1644
mgadzhi <musaevgadzhi@gmail.com>
2016-05-05 20:50:01 +0200

    Refactoring

--- scripts/produce_poems.py
+++ scripts/produce_poems.py
@@ -6,6 +6,7 @@ import sys
 import random
 
 from news2lyric.grammar import parse_sentence, get_subject, get_verbs
+from news2lyric.poems import produce_poem_with_line
 from news2lyric.structures import guinea_pig
 
 
@@ -43,45 +44,6 @@ def shuffle_keys_ngrams(file_paths, rhyme_structure):
             with open(v) as handle:
                 structure_keys_to_ngrams[k] = [line.strip() for line in handle.readlines()]
     return structure_keys_to_ngrams
-
-
-def is_good_enough(poem):
-    return len(set(line.split(' ')[-1] for line in poem)) == len(poem)
-
-
-def produce_poem_with_line(file_paths, rhyme_structure, line_fname, found_words_title, subj_or_verb_line=None):
-    print('line_fname = {}'.format(line_fname))
-    print('subj_or_verb_l = {}'.format(subj_or_verb_line))
-    dirname = os.path.dirname(file_paths[0])
-    poem = [line_fname[0]]
-    with open(os.path.join(dirname, line_fname[1])) as line_rhymes_file:
-        line_rhymes = [l.strip() for l in line_rhymes_file]
-    line_rhyme = random.choice(line_rhymes)
-    while line_fname[0].split(' ')[-1] == line_rhyme.split(' ')[-1]:
-        line_rhyme = random.choice(line_rhymes)
-    poem.append(line_rhyme)
-    print poem
-    if found_words_title == 'subj&verb':
-        next_file = random.choice(file_paths)
-        with open(next_file) as nf:
-            next_line_candidates = [l.strip() for l in nf]
-            line3 = random.choice(next_line_candidates)
-            line4 = random.choice(next_line_candidates)
-            while line3.split(' ')[-1] == line4.split(' ')[-1]:
-                line4 = random.choice(next_line_candidates)
-        poem.append(line3)
-        poem.append(line4)
-    if found_words_title == 'subj':
-        line3 = subj_or_verb_line[0]
-        with open(os.path.join(dirname, subj_or_verb_line[1])) as line_rhymes_file:
-            line_rhymes = [l.strip() for l in line_rhymes_file]
-        line4 = random.choice(line_rhymes)
-        while subj_or_verb_line[0].split(' ')[-1] == line4.split(' ')[-1]:
-            line4 = random.choice(line_rhymes)
-        poem.append(line3)
-        poem.append(line4)
-    print poem
-    return poem
 
 
 def main():
